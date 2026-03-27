fccd23c8381f060ae9021e673faba362e412d74d
robotmaster <64709738+robotmaster@users.noreply.github.com>
2026-03-27 11:41:14 -0700

    improved wall animation

--- 3d-scary/scripts/placeable_wall.gd
+++ 3d-scary/scripts/placeable_wall.gd
@@ -1,9 +1,14 @@
 extends StaticBody3D
 
-
+@export var WALL_RISE_TIME = 0.3
 func _ready() -> void:
+	#wall_rise_timer = WALL_RISE_TIME
 	var Floor : FloorType = get_node("/root/Main/Floor")
 	$WallMesh.mesh.size.x = Floor.LINE_SIZE
 	$WallMesh.mesh.size.z = Floor.TILE_SIZE + Floor.LINE_SIZE
 	
 	$WallCollision.shape.size = $WallMesh.mesh.size
+	
+	$WallMesh.global_position.y = -$WallMesh.mesh.size.y / 2
+	var new_tween = get_tree().create_tween().set_trans(Tween.TRANS_EXPO)
+	new_tween.tween_property($WallMesh, "global_position:y", 0, WALL_RISE_TIME)
